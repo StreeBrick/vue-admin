c2dc54ac865b121800858202990288014d462574
StreeBrick <565463222@qq.com>
2020-11-22 15:39:15 +0800

    新建项目

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login">
+        fdsa 
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
+<style scoped>
+#login{
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
